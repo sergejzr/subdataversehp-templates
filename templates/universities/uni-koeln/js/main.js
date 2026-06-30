d245a2962f90225900bb808b4cb332c04ad833f7
Flemming Reese <freese@uni-bonn.de>
2026-06-30 10:27:23 +0200

    Add uni-koeln theming (adapted+custom, CD colors, self-hosted fonts, Sphären-BG)

---
+++ templates/universities/uni-koeln/js/main.js
@@ -0,0 +1,18 @@
+     var Hyphenopoly = {
+                                    require: {
+                                        "de": "Silbentrennungsalgorithmus",
+                                        "en-us": "Supercalifragilisticexpialidocious"
+                                    },
+                                    setup: {
+                                        dontHyphenate: {
+                                            noscript: true,
+                                        },
+                                        dontHyphenateClass: "ubf-dont-hyphenate",
+                                        selectors: {
+                                            ".ubf-mainMenu": {},
+                                            ".ubf-section": {},
+                                            ".ubf-secondaryMenu__item": {},
+                                            ".ubf-footer__menuItem": {}
+                                        }
+                                    }
+                                    };
